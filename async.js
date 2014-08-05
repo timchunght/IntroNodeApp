4dc833b9e37cc9857ae807be80d806342faf6e37
ScottDalessandro <scott@howdy-tech.com>
2014-08-05 10:35:13 -0400

    Updated async.js file code

--- async.js
+++ async.js
@@ -1,10 +1,11 @@
-console.log("Node starting...");
+var nodeStartingFunctionFileCode = require("./nodeStartingFunction");
+nodeStartingFunctionFileCode.nodeStartingFunction();
 var fs = require('fs');
-                           // callback function
 fs.readFile('./file1.txt', function(err, data) {
     console.log(data);  // buffer object
     console.log(data.toString());
 });
+
 
 
 
